c30ed717beebede2527996d4fc4f4f35cd04eb1d
Scott <scottbryan17@gmail.com>
2017-05-09 15:36:37 +0800

    updated

--- e-Library/deletebook.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "database.php";
-session_start();
-
-	$deletebook=mysql_query("delete from book where BookID='$_GET[deletebid]'");
-	header("location:booklist.php");
-
-?>
--- e-Library/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION['NoValue']="";
-$_SESSION['UserID']="";
-$_SESSION['Position']="Visitor";
-header("Location:login.php");
-?>
